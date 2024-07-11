865f05c7f96f3234f6448ac8ff867605ba5c63bf
Mikkel Hansen <me@thisdevdane.com>
2024-07-11 20:52:29 +0200

    fix typo

--- run_after_linux-002-setup-nvim.sh
+++ run_after_linux-002-setup-nvim.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure config folder exists
-if [ -d ~/.config/nvim ]; then
+if [ ! -d ~/.config/nvim ]; then
     git clone github.com/ThisDevDane/nvim-config ~/.config/nvim
 else
     cd ~/.config/nvim && git pull
