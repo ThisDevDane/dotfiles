c19301e6e844407561b939fab62a8d806b44779b
Mikkel Hansen <me@thisdevdane.com>
2024-08-30 17:52:05 +0200

    checkpoint

--- run_after_linux-002-setup-nvim.sh
+++ run_after_linux-002-setup-nvim.sh
@@ -2,7 +2,7 @@
 
 # Make sure config folder exists
 if [ ! -d ~/.config/nvim ]; then
-    git clone git@github.com:ThisDevDane/nvim-config.git ~/.config/nvim
+    git clone -b lazy-v2 git@github.com:ThisDevDane/nvim-config.git ~/.config/nvim
 else
     cd ~/.config/nvim && git pull
 fi
